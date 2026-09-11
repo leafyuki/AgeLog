75d358e4efcf24c22d3e35da2a9af198a544fa79
leafyuki <yuki.yuki.cream@gmail.com>
2026-09-12 06:34:24 +0900

    ソース管理

--- sw.js
+++ sw.js
@@ -1,8 +1,39 @@
-const CACHE = 'agelog-v9';
-const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.svg', './icon-512.svg'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())));
-self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
+const CACHE = 'agelog-v10';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => e.waitUntil(
+  caches.open(CACHE)
+    .then(c => c.addAll(ASSETS))
+    .then(() => self.skipWaiting())
+));
+
+self.addEventListener('activate', e => e.waitUntil(
+  caches.keys()
+    .then(keys => Promise.all(
+      keys
+        .filter(k => k !== CACHE)
+        .map(k => caches.delete(k))
+    ))
+    .then(() => self.clients.claim())
+));
+
 self.addEventListener('fetch', e => {
   if (e.request.method !== 'GET') return;
-  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request).then(r => { const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r; }).catch(() => caches.match('./index.html'))));
+
+  e.respondWith(
+    caches.match(e.request).then(cached =>
+      cached || fetch(e.request).then(r => {
+        const copy = r.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return r;
+      }).catch(() => caches.match('./index.html'))
+    )
+  );
 });
